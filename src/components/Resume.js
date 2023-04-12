ee859b720f01fbd59fa756c5cc463f708a1b859a
Sebastian Tischner <alexanders.tischner@outlook.com>
2023-04-11 23:33:37 -0600

    change to resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,6 +1,6 @@
 import { Col, Container, Row } from "react-bootstrap"
 import roverImg from "../assets/images/LunarRover.png"
-import resumeImg from "../assets/images/Sebastian-Resume"
+import resumeImg from "../assets/images/Sebastian-Resume1.png"
 import { Link } from 'react-router-dom';
 
 
